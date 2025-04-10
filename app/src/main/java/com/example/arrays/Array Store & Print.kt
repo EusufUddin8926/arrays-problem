146986daf040dfb6ed60a33b3f6a12858acbff29
Md Eusuf Uddin <eusuf.uddin@v2.ltd>
2025-04-10 23:00:29 +0600

    Array reverse display

--- app/src/main/java/com/example/arrays/Array Store & Print.kt
+++ app/src/main/java/com/example/arrays/Array Store & Print.kt
@@ -1,5 +1,13 @@
 package com.example.arrays
 
 fun main(){
-    
+    var i = 0
+    val array = IntArray(10)
+    while (i < 10){
+        val input = readln().toInt()
+        array[i] = input
+        i++
+    }
+
+    println(array.joinToString())
 }
